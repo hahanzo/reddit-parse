d81a2928b4b5ee2e5e12b79a2e3f2088ad5a8b43
olegdrobit <olegdrobit16@gmail.com>
2023-10-28 19:06:09 +0300

    add file_name argument to scrape function

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 import time
 import json
 import csv
+import math
 from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -8,12 +9,11 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 # enable the headless mode
 options = Options()
 options.add_argument('--headless')
+options.add_argument('--disable-blink-features=AutomationControlled')
 
 # initialize a web driver to control Chrome
 driver = webdriver.Chrome(
@@ -51,9 +51,12 @@ def get_post_info(url):
             .find_element(By.CLASS_NAME, "score.unvoted")  \
             .text
         
-        author = post_html_element \
-            .find_element(By.CLASS_NAME, "author") \
-            .text
+        try:
+            author = post_html_element \
+                .find_element(By.CLASS_NAME, "author") \
+                .text
+        except:
+            author = '[Deleted]'
         
         datatime = post_html_element \
             .find_element(By.CLASS_NAME, "tagline") \
@@ -84,7 +87,7 @@ def get_post_info(url):
         # to the list of scraped posts
         posts.append(post)
 
-def scrape_subreddit(subreddit_url,page_amount):
+def scrape_subreddit(subreddit_url,page_amount,file_name):
     # initialize the page number
     page_number = 0
     # initialize the page URL
@@ -95,27 +98,40 @@ def scrape_subreddit(subreddit_url,page_amount):
     while page_number < page_amount:
         # get the post info for the current page
         get_post_info(page_url)
+        
         page_number += 1
-        # refresh page 
-        page_url = driver \
-            .find_element(By.CSS_SELECTOR, 'span.next-button') \
-            .find_element(By.TAG_NAME, 'a') \
-            .get_attribute('href')
+        
+        time.sleep(2)
+        # refresh page
+        try:
+            page_url = driver \
+                .find_element(By.CSS_SELECTOR, 'span.next-button') \
+                .find_element(By.TAG_NAME, 'a') \
+                .get_attribute('href')
+        except:
+            print("no next page")
+            break
+
+        print("page",page_number)
 
     # export the scraped data to a JSON file
-    with open('subreddit.json', 'w', encoding='utf-8') as file:
+    with open(f'{file_name}.json', 'w', encoding='utf-8') as file:
         json.dump({'posts': posts}, file, indent=4, ensure_ascii=False)
     
     # Export the scraped data to a CSV file
-    with open('subreddit.csv', 'w', newline='', encoding='utf-8') as csv_file:
+    with open(f'{file_name}.csv', 'w', newline='', encoding='utf-8') as csv_file:
         csv_writer = csv.DictWriter(csv_file, fieldnames=posts[0].keys())
         csv_writer.writeheader()
         csv_writer.writerows(posts)
     # close the browser and free up the Selenium resources
     driver.quit()
 
-# the URL of the target page to scrape
-url = 'https://old.reddit.com/top/?sort=top&t=month'
+# get 'top month' subreddit info
+#scrape_subreddit('https://old.reddit.com/top/?sort=top&t=month',math.inf,'top_month_subreddit')
+# get 'top year' subreddit info
+#scrape_subreddit('https://old.reddit.com/top/?sort=top&t=year',math.inf,'top_year_subreddit')
+# get 'top all' subreddit info
+#scrape_subreddit('https://old.reddit.com/top/?sort=top&t=all',math.inf,'top_all_subreddit')
 
-scrape_subreddit(url,2)
-
+scrape_subreddit('https://old.reddit.com/r/popular/?geo_filter=GLOBAL',2,'popular_everywhera_subreddit')
+scrape_subreddit('https://old.reddit.com/r/popular/?geo_filter=AR',2,'popular_ar_subreddit')
